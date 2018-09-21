966557424fdfab41ac771ed200e7f5ff7d881c24
DaQuiTree <dhsxmy1314@163.com>
2018-09-21 13:58:41 +0800

    update eclipse setting

--- examples/mqtt/build/aliyun/component_project_vars.mk
+++ examples/mqtt/build/aliyun/component_project_vars.mk
@@ -1,6 +1,6 @@
 # Automatically generated build file. Do not edit.
-COMPONENT_INCLUDES += /home/daquitree/esp/esp-aliyun/components/aliyun/iotkit-embedded/src/sdk-impl /home/daquitree/esp/esp-aliyun/components/aliyun/iotkit-embedded/src/sdk-impl/exports /home/daquitree/esp/esp-aliyun/components/aliyun/iotkit-embedded/src/sdk-impl/imports /home/daquitree/esp/esp-aliyun/components/aliyun/iotkit-embedded/src/packages/LITE-utils /home/daquitree/esp/esp-aliyun/components/aliyun/iotkit-embedded/src/log/LITE-log
-COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/aliyun -laliyun -L /home/daquitree/esp/esp-aliyun/components/aliyun/iotkit-embedded/output/release/lib -liot_sdk
+COMPONENT_INCLUDES += /home/daquitree/Github/ESP32_SmartConfig_Temperature_AliyunIOT/components/aliyun/iotkit-embedded/src/sdk-impl /home/daquitree/Github/ESP32_SmartConfig_Temperature_AliyunIOT/components/aliyun/iotkit-embedded/src/sdk-impl/exports /home/daquitree/Github/ESP32_SmartConfig_Temperature_AliyunIOT/components/aliyun/iotkit-embedded/src/sdk-impl/imports /home/daquitree/Github/ESP32_SmartConfig_Temperature_AliyunIOT/components/aliyun/iotkit-embedded/src/packages/LITE-utils /home/daquitree/Github/ESP32_SmartConfig_Temperature_AliyunIOT/components/aliyun/iotkit-embedded/src/log/LITE-log
+COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/aliyun -laliyun -L /home/daquitree/Github/ESP32_SmartConfig_Temperature_AliyunIOT/components/aliyun/iotkit-embedded/output/release/lib -liot_sdk
 COMPONENT_LINKER_DEPS += 
 COMPONENT_SUBMODULES += 
 COMPONENT_LIBRARIES += aliyun
